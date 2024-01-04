5cfc5b387c2dbcc10a7d2efb1f389af4a4f28604
piksel-man231 <piksel-man231@ya.ru>
2024-01-04 14:58:47 +0000

    enden one message frontend

--- weather api.py
+++ weather api.py
@@ -42,7 +42,7 @@ def get_text_messages(message):
         # Преобразуем ответ в JSON формат 
             data = response.json() 
             # Выводим данные о текущей погоде 
-            bot.send_message(message.from_user.id, f'Температура воздуха: {data["fact"]["temp"]} °C\nОщущается как: {data["fact"]["feels_like"]} °C\nПогодное описание: {data["fact"]["condition"]}') 
+            #bot.send_message(message.from_user.id, f'Температура воздуха: {data["fact"]["temp"]} °C\nОщущается как: {data["fact"]["feels_like"]} °C\nПогодное описание: {data["fact"]["condition"]}') 
             #Считаем суточные осадки
             prec_total = 0
             today_prec_morning = data["forecasts"]["date" == now]["parts"]["morning"]["prec_mm"]
@@ -53,11 +53,16 @@ def get_text_messages(message):
             #print(today_prec_night)
             prec_total = today_prec_morning + today_prec_day + today_prec_night
             #print(prec_total)
-            bot.send_message (message.from_user.id, f'Ожидаемые осадки сегодня: ' + str(prec_total) + 'mm')
+            
             if  prec_total == 0:
-                bot.send_message(message.from_user.id, 'Сегодня можно и на мойку заехать')
+ # Выводим данные о текущей погоде 
+                bot.send_message(message.from_user.id, f'Температура воздуха: {data["fact"]["temp"]} °C\nОщущается как: {data["fact"]["feels_like"]} °C\nПогодное описание: {data["fact"]["condition"]}\nОжидаемые осадки сегодня: ' + str(prec_total) + 'mm\nСегодня можно и на мойку заехать') 
+                #bot.send_message(message.from_user.id, f'Ожидаемые осадки сегодня: ' + str(prec_total) + 'mm')
+                #bot.send_message(message.from_user.id, 'Сегодня можно и на мойку заехать')
             else:
-                bot.send_message(message.from_user.id, 'Ну помытся конечно можно, но ты дороги видел?')
+                bot.send_message(message.from_user.id, f'Температура воздуха: {data["fact"]["temp"]} °C\nОщущается как: {data["fact"]["feels_like"]} °C\nПогодное описание: {data["fact"]["condition"]}\nОжидаемые осадки сегодня: ' + str(prec_total) + 'mm\nНу помытся конечно можно, но ты дороги видел? ') 
+                #bot.send_message(message.from_user.id, f'Ожидаемые осадки сегодня: ' + str(prec_total) + 'mm')
+                #bot.send_message(message.from_user.id, 'Ну помытся конечно можно, но ты дороги видел?')
             bot.send_message(message.from_user.id, 'Данные получены из сервиса Яндекс.Погода')
         else: 
             # Выводим код ошибки 
